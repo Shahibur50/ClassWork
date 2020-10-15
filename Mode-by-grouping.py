eb4d1a5502db3cb03a9d987798ffc35cb72dfc6b
Shahibur Rahaman <shahibur46@gmail.com>
2020-10-15 13:57:13 +0530

    Update Mode-by-grouping.py

--- Mode-by-grouping.py
+++ Mode-by-grouping.py
@@ -1,151 +1,209 @@
 """
-Mode by grouping version 1.5.10.20
+Mode by grouping version 1.7.10.20
 
 Copyright (c) 2020 Shahibur Rahaman
 Licensed under MIT
 """
 
-import statistics  # Importing/adding a module which gives us extra functionality.
-
-
-size = ["0-5", "5-10", "10-15", "15-20", "20-25", "25-30", "30-35", "35-40"]  # Class Interval
-freq = [3, 7, 15, 30, 20, 10, 5, 15]  # Frequency
- 
-print(f"Class interval: {size}")
-print(f"Frequency: {freq}\n")
-
-print("-" * 50)
-print("I Group")
-
-m0 = []  # Creating an array/list where the values can be stored.
-
-for i in range(len(size)):
-    group0 = freq[i]
-    m0.append(group0)
-
-mode0 = max(m0)  # Getting the biggest value from the array/list.
-m0_index = m0.index(mode0)  # Getting index number related to the frequency of rescpective class.
-
-print(m0)
-print("\nBiggest class interval for this group:", big_size0 := size[m0_index])
-
-
-print("-" * 50)
-print("II Group")
+import statistics
+import time
 
 m1 = []
-
-for i in range(0, len(size) - 1, 2):
-    group1 = freq[i] + freq[i + 1]
-    m1.append(group1)
-
-mode1 = max(m1)
-m1_index = m1.index(mode1)
-
-print(m1)
-print("\nBiggest class interval for this group:", big_size1 := size[m1_index * 2 + 1])
-
-
-print("-" * 50)
-print("III Group")
-
 m2 = []
-
-for i in range(1, len(size) - 1, 2):
-    group2 = freq[i] + freq[i + 1]
-    m2.append(group2)
-
-mode2 = max(m2)
-m2_index = m2.index(mode2)
-
-print(m2)
-print("\nBiggest class interval for this group:", big_size2 := size[m2_index * 2 + 2])
-
-
-print("-" * 50)
-print("IV Group")
 m3 = []
-
-for i in range(0, len(size) - 2, 3):
-    group3 = freq[i] + freq[i + 1] + freq[i + 2]
-    m3.append(group3)
-
-mode3 = max(m3)
-m3_index = m3.index(mode3)
-
-print(m3)
-print("\nBiggest class interval for this group:", big_size3 := size[m3_index * 2 + 3])
-
-
-print("-" * 50)
-print("V Group")
 m4 = []
-
-for i in range(1, len(size) - 2, 3):
-    group4 = freq[i] + freq[i + 1] + freq[i + 2]
-    m4.append(group4)
-
-mode4 = max(m4)
-m4_index = m4.index(mode4)
-
-print(m4)
-print("\nBiggest class interval for this group:", big_size4 := size[m4_index * 2 + 3])
-
-
-print("-" * 50)
-print("VI Group")
 m5 = []
+m6 = []
+ms = [m1, m2, m3, m4, m5, m6]
 
-for i in range(2, len(size) - 2 , 3):
-    group5 = freq[i] + freq[i + 1] + freq[i + 2]
-    m5.append(group5)
+interv = ['', '', '', '', '', '']
+modal = 0
 
-mode5 = max(m5)
-m5_index = m5.index(mode5)
 
-print(m5)
-print("\nBiggest class interval for this group:", big_size5 := size[m5_index * 2 + 3])
-print("-" * 50)
+def main():
+    print("Starting up", end="")
+    for i in range(3):
+        print('.', end="")
+        time.sleep(1)
+    print("")
+    
+    table(size, freq)
+    estimation_table()
+    analyze()
+    
+    print("_________________________________________")
+    print("Calculating the mode of given data...")
+    time.sleep(1)
+    print("Please wait", end="")
+    for i in range(3):
+        print('.', end="")
+        time.sleep(1)
+    print("")
+    print(f"\nThe mode of given data is |> {mode()} <|.\n")
 
-biggest_sizes = [big_size0, big_size1, big_size2, big_size3, big_size4, big_size5]
 
-print(f"\nBiggest class intervals occuring in all of the groups are {biggest_sizes}.\n")
+def table(intervals, freq):
+    print("________________________________")
+    print("             TABLE")
+    print("________________________________")
+    j = 0
+    print("CLASS INTERVALS        FREQUENCY")
+    for i in intervals:
+        if len(i) <= 3:
+            space = 2
+        elif len(i) <= 4:
+            space = 1
+        else:
+            space = 0
+        print("   ", i, " " * (15 + space), freq[j])
+        j += 1
+    print("")
 
-modal = statistics.mode(biggest_sizes)
-print(f"Therefore the modal class of given data is {modal}.")
 
-# Setting up the values required for calculating the mode of the modal class.
+def estimation_table():
+    global interv
+    print("________________________________")
+    print("           ESTIMATION")
+    print("________________________________")
+    print("\nGROUPS", " " * 2, "BIG. VALUES     CLASS \n")
+    biggest_classes = [group1(size, freq), group2(size, freq), group3(size, freq),
+                      group4(size, freq), group5(size, freq), group6(size, freq)]
+    j = 0
+    for i in biggest_classes:
+        if j == 0:
+            interv[j] = size[ms[j].index(i)]
+            print("GROUP", j + 1, " " * 5, i, " " * 8, interv[j])
+        elif j == 1:
+            interv[j] = size[ms[j].index(i) * 2 + 1]
+            print("GROUP", j + 1, " " * 5, i, " " * 8, interv[j])
+        elif j == 2:
+            interv[j] = size[ms[j].index(i) * 2 + 2]
+            print("GROUP", j + 1, " " * 5, i, " " * 8, interv[j])
+        else:
+            interv[j] = size[ms[j].index(i) * 2 + 3]
+            print("GROUP", j + 1, " " * 5, i, " " * 8, interv[j])
+        j += 1
 
-l1 = ""
-h2 = ""
-for i in modal:
-    if i == "-":
-        break
-    else:
-        l1 += i
-h1 = l1
 
-for i in range(len(modal)):
-    i += 1
-    if modal[-i] == "-":
-        break
-    else:
-        h2 += modal[-i]
+def analyze():
+    global interv
+    global modal
 
-h2 = int(h2[::-1])
-h1 = int(h1)
-h = h2 - h1
+    print("_________________________________________")
+    print("               ANALYSIS")
+    print("_________________________________________")
+    print("        BIGGEST CLASSES IN GROUPS\n")
+    for i in range(len(interv)):
+        if i == len(interv) - 1:
+            print(interv[i])
+        else:
+            print(interv[i], end = ", ")
 
-l1 = int(l1)
+    modal = statistics.mode(interv)
+    print(f"\n=> The modal class after analyzing the data is {modal}.")
 
-f1 = freq[size.index(modal)]
 
-f0 = freq[size.index(modal) - 1]
+def mode():
+    global modal
+    global freq
 
-f2 = freq[size.index(modal) + 1]
+    freq.append(0)
+    freq.insert(freq[0], 0)
 
-# Calculating mode.
+    l1 = ""
+    h2 = ""
+    for i in modal:
+        if i == "-":
+            break
+        else:
+            l1 += i
+    h1 = l1
 
-Z = l1 + ((f1 - f0) / (2 * f1 - f0 - f2) * h)
-print(f"\nThe mode of given data is {Z}.")
-print("\nCalculations Done.")
+    for i in range(len(modal)):
+        i += 1
+        if modal[-i] == "-":
+            break
+        else:
+            h2 += modal[-i]
+
+    h2 = int(h2[::-1])
+    h1 = int(h1)
+    h = h2 - h1
+
+    l1 = int(l1)
+
+    f1 = freq[size.index(modal)]
+
+    f0 = freq[size.index(modal) - 1]
+
+    f2 = freq[size.index(modal) + 1]
+
+    # Calculating mode.
+
+    z = l1 + ((f1 - f0) / (2 * f1 - f0 - f2) * h)
+    return z
+
+def group1(intervals, freq):
+    global m1
+    for i in range(len(intervals)):
+        group = freq[i]
+        m1.append(group)
+
+    mode = max(m1)
+    return mode
+
+
+def group2(intervals, freq):
+    global m2
+    for i in range(0, len(intervals) - 1, 2):
+        group = freq[i] + freq[i + 1]
+        m2.append(group)
+
+    mode = max(m2)
+    return mode
+
+
+def group3(intervals, freq):
+    global m3
+    for i in range(1, len(intervals) - 1, 2):
+        group = freq[i] + freq[i + 1]
+        m3.append(group)
+
+    mode = max(m3)
+    return mode
+
+
+def group4(intervals, freq):
+    global m4
+    for i in range(0, len(intervals) - 2, 3):
+        group = freq[i] + freq[i + 1] + freq[i + 2]
+        m4.append(group)
+
+    mode = max(m4)
+    return mode
+
+
+def group5(intervals, freq):
+    global m5
+    for i in range(1, len(intervals) - 2, 3):
+        group = freq[i] + freq[i + 1] + freq[i + 2]
+        m5.append(group)
+
+    mode = max(m5)
+    return mode
+
+
+def group6(intervals, freq):
+    global m6
+    for i in range(2, len(intervals) - 2, 3):
+        group = freq[i] + freq[i + 1] + freq[i + 2]
+        m6.append(group)
+
+    mode = max(m6)
+    return mode
+
+
+if __name__ == "__main__":
+    size = ["0-5", "5-10", "10-15", "15-20", "20-25", "25-30", "30-35", '35-40', '40-45', '45-50', '50-55', '55-60']  # Class Interval
+    freq = [3, 7, 15, 30, 20, 10, 5, 5, 3, 1, 6, 50]  # Frequency
+    main()
